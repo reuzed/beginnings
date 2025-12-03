94c6dba9807b75e4fe0be37e791b9d39b025c4a9
reuzed <49994279+reuzed@users.noreply.github.com>
2025-12-03 22:23:00 +0000

    progress killing sorrys

--- InfinitePrimes2.lean
+++ InfinitePrimes2.lean
@@ -58,7 +58,50 @@ lemma product_fermat (k : ℕ) : ∏ i < k, fermat i + 2 = fermat k := by
   · ext
     simp
 
-lemma blah (m n p q : ℕ)(hmn : m ≠ n)() m ≠ n → p ∣ fermat n → q → ∣
+lemma fermat_neq_1 (m : ℕ) : fermat m ≠ 1 := by
+  unfold fermat
+  simp
+
+lemma n_in_finset_m (n m : ℕ) (hmn : m > n) : n ∈ Finset.Iio m := by
+  simp [hmn]
+
+lemma finset_m_eq_n_union_rest (n m : ℕ) (hmn : m > n)
+    : Finset.Iio m = {n} ∪ (Finset.Iio n ∪ Finset.Ioo n m) := by
+  ext a
+  simp
+  cutsat
+
+lemma prod_finset_m_eq (n m : ℕ) (hmn : m > n)
+    : ∏ i ∈ (Finset.Iio m), fermat i =
+    fermat n * (∏ i ∈ (Finset.Iio n ∪ Finset.Ioo n m), fermat i) := by
+  rw [finset_m_eq_n_union_rest n m hmn]
+  rw [←Finset.prod_singleton fermat n]
+  apply Finset.prod_union ?_
+  simp
+
+lemma f_n_dvd_prod_f_i (n m : ℕ) (hmn : m > n) : fermat n ∣ ∏ i < m, fermat i := by
+  rw [prod_finset_m_eq n m hmn]
+  simp
+
+
+lemma unique_factor_2 (p : ℕ) (hpt : p ∣ 2) (hpp : Nat.Prime p) : p = 2 := by
+  sorry
+
+lemma distinct_fermat_gcd_1 (m n p : ℕ) (hmn : m > n)
+    (hp : Nat.Prime p) (hpn : p ∣ fermat n) (hqm : p ∣ fermat m)
+    : p = 2 := by
+  have hpm : p ∣ ∏ i < m, fermat i := Nat.dvd_trans hpn (f_n_dvd_prod_f_i n m hmn)
+  have hprod_le_fm : ∏ i < m, fermat i ≤ fermat m := by
+    rw [←product_fermat m]
+    simp
+  have pd2_ish := (Iff.mp (Iff.symm (Nat.dvd_sub_iff_left hprod_le_fm hpm))) hqm
+
+  simp [←product_fermat m] at pd2_ish
+
+  exact unique_factor_2 p pd2_ish hp
+
+lemma two_ndivd_fermat (m : ℕ) : ¬2 ∣ 2 ^ 2 ^ m + 1 := by
+  sorry
 
 lemma fermat_minfac_distinct (m n : ℕ) : m ≠ n → (fermat m).minFac ≠ (fermat n).minFac := by
   intro hmn
@@ -71,12 +114,21 @@ lemma fermat_minfac_distinct (m n : ℕ) : m ≠ n → (fermat m).minFac ≠ (fe
       assumption
     · cutsat
   · simp
-    rw [←product_fermat m]
     intro hferm
+    have hlol : (fermat m).minFac ≠ 2 := by
+      by_contra h2
+      have h3 : _ :=Nat.minFac_dvd (fermat m)
+      rw [h2] at h3
+      unfold fermat at h3
+      apply two_ndivd_fermat m
+      assumption
 
-
-
-
+    apply hlol
+    apply distinct_fermat_gcd_1 m n (fermat m).minFac h
+    · exact Nat.minFac_prime (fermat_neq_1 m)
+    · rw [hferm]
+      exact Nat.minFac_dvd (fermat n)
+    · exact Nat.minFac_dvd (fermat m)
 
 theorem infinite_sequence_of_primes : ∃ f : ℕ → ℕ, f.Injective ∧ ∀ n : ℕ, Nat.Prime (f n) := by
   let f : ℕ → ℕ := fun n ↦ Nat.minFac (fermat n)
